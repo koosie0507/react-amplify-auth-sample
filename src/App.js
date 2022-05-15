4f529617b3342b3feaa788176ad56447bb69d7f1
Andrei Olar <andrei.olar@complyadvantage.com>
2022-05-15 14:49:06 +0300

    Add Amplify to the mix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import {Authenticator, View} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // default theme
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Authenticator.Provider>
+            <View>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </View>
+        </Authenticator.Provider>
+    );
 }
 
 export default App;
